71f8f0be4485d47c943a27bcbacefc7e08671b8c
Jeremiah <jclanc7507@gmail.com>
2022-03-04 14:03:43 -0500

    coded out battle & actions; needs debugging

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const Game = require('./lib/Game');
+const Player = require('./lib/Player');
 
-new Game().initializeGame();
+new Game().initializeGame() 
+    .then(({ name }) => {
+        this.player = new Player(name);
+
+        this.startNewBattle()             
+            console.log('Your stats are as follows:');
+            console.table(this.player.getStats());
+            console.log(this.currentEnemy.getDescription());
+            this.battle()
+    });
