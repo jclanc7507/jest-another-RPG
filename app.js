7c1e43117e96c2ac5cf35800473434a1b1c90bc1
Jeremiah <jclanc7507@gmail.com>
2022-03-04 15:02:07 -0500

    debugged game, runs successfully, can't kill the orc

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 const Game = require('./lib/Game');
 const Player = require('./lib/Player');
 
-new Game().initializeGame() 
-    .then(({ name }) => {
-        this.player = new Player(name);
-
-        this.startNewBattle()             
-            console.log('Your stats are as follows:');
-            console.table(this.player.getStats());
-            console.log(this.currentEnemy.getDescription());
-            this.battle()
-    });
+new Game().initializeGame();
